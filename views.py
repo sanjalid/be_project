63152e1237a1c3fc9e75a7e4343f6ef3cd974b6e
sanjalid <40570371+sanjalid@users.noreply.github.com>
2020-03-16 12:01:10 +0530

    Delete views.py

--- views.py
+++
@@ -1,7 +0,0 @@
-# Create your views here.
-from django.shortcuts import render 
-from django.http import HttpResponse # Create your views here. 
-def index(request): 
-    return render(request, 'index.html')
-def login(request): 
-    return render(request, 'login.html')
